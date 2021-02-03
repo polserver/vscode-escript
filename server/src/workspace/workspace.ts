d904178d6e693ff30d4bb6e02d588933ebe39c1e
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2021-02-03 22:51:23 +0100

    Workspace loading (#1)
    * Initial workspace load
    
    * Caching of workspace loading
    
    * Load workspace on document change
    
    * Use fsPath and not path
    
    * Fix packaging
    
    * Remove test code

---
+++ server/src/workspace/workspace.ts
@@ -0,0 +1,171 @@
+import { dirname, join } from 'path';
+import { promises } from 'fs';
+import readFile = promises.readFile;
+import access = promises.access;
+
+/**
+ * Workspace configuration object.
+ */
+type WorkspaceConfig = {
+    polRoot: string;
+    moduleDirectory: string;
+    includeDirectory: string;
+    polScriptRoot: string;
+    /** May be zero-length */
+    packageRoots: string[];
+}
+
+class CfgFileReader {
+    /**
+     * Returns a `Promise` that resolves to an object which represents the a
+     * .cfg files contents:
+     *   - the cfg file's keys are lowercased.
+     *   - the values are all a `string[]`, to allow multiple values.
+     * @param path File to load
+     * @throws All `fs`-related exceptions
+     * @throws `parseAsInner=false not yet supported
+     */
+    static async load(path: string, parseAsInner = false) {
+
+        const contents = await readFile(path, 'utf-8');
+        if (parseAsInner) {
+            // The return value
+            const ret: { [key: string]: Array<string> | undefined } = {};
+
+            for (const line of contents.split(/[\r\n]+/)) {
+                // Skip comments
+                if (line.startsWith('#')) {
+                    continue;
+                }
+                const matches = line.match(/^([^=\s]+)\s*(?:=\s*)?(.+)$/);
+                if (matches) {
+                    const key = matches[1].toLowerCase();
+                    const value = matches[2];
+                    const existing = ret[key];
+                    if (existing) {
+                        existing.push(value);
+                    } else {
+                        ret[key] = [value];
+                    }
+                }
+            }
+            return ret;
+        }
+        throw new Error('parseAsInner=false not yet supported');
+    }
+}
+
+export class Workspace {
+    /**
+     * Cache to hold workspaces. Key is the POL root, ie. `key ===
+     * value.polRoot`
+     */
+    private static workspaces: Map<string, Workspace> = new Map();
+
+    /**
+     * Constructs a new `Workspace`. This method is private, as the public
+     * interface is {@link `Workspace.find`}.
+     * @param config Workspace configuration
+     */
+    private constructor(private config: WorkspaceConfig) {
+    }
+
+    /**
+     * Returns a Workspace for a given script. Will return `undefined` if none
+     * found. See internal implementation in {@link Workspace._find2}.
+     * @param path Path of file (script, include, ...)
+     */
+    public static find(path: string) {
+        return this.addTask(path);
+    }
+
+
+    /**
+     * Task executor. Used to sequentialize calls to {@link Workspace._find2}
+     */
+    private static addTask = (() => {
+        let pending = Promise.resolve<Workspace | undefined>(undefined);
+
+        const run = async (path: string) => {
+            try {
+                await pending;
+            } finally {
+                return Workspace._find(path);
+            }
+        };
+
+        // Update pending promise so that next task could await for it
+        return (path: string) => (pending = run(path));
+    })();
+
+    /**
+     * Returns a Workspace for a given script. Will return `undefined` if none
+     * found.
+     * @param path Path of file (script, include, ...)
+     */
+    static async _find(path: string): Promise<Workspace | undefined> {
+        try {
+            let lastPath = path;
+            for (const [polRoot, workspace] of this.workspaces.entries()) {
+                if (path.startsWith(polRoot)) {
+                    return workspace;
+                }
+            }
+            while (true) {
+                try {
+                // Check if ./pol.cfg exists. Is this needed...?
+                    await access(join(path, 'pol.cfg'));
+
+                    // Read ./scripts/ecompile.cfg
+                    const cfg = await CfgFileReader.load(join(path, 'scripts', 'ecompile.cfg'), true);
+                    const polRoot = path;
+                    const moduleDirectory = cfg.moduledirectory?.[0];
+                    const includeDirectory = cfg.includedirectory?.[0];
+                    const polScriptRoot = cfg.polscriptroot?.[0];
+                    const packageRoots = cfg.packageroot;
+
+                    if (!moduleDirectory || !includeDirectory || !polScriptRoot) {
+                        continue;
+                    }
+
+                    // Check the existence of these folders
+                    await access(moduleDirectory);
+                    await access(includeDirectory);
+                    await access(polScriptRoot);
+
+                    const config: WorkspaceConfig = {
+                        polRoot,
+                        includeDirectory,
+                        moduleDirectory,
+                        polScriptRoot,
+                        packageRoots: []
+                    };
+
+                    // Package roots are technically optional for a `Workspace`.
+                    if (packageRoots) {
+                        for (const packageRoot of packageRoots) {
+                            try {
+                                await access(packageRoot);
+                                config.packageRoots.push(packageRoot);
+                            } catch {
+                            // Ignore this PackageRoot
+                            }
+                        }
+                    }
+
+                    const workspace = new Workspace(config);
+                    Workspace.workspaces.set(polRoot, workspace);
+                    return workspace;
+                } catch (e) {
+                }
+                path = dirname(path);
+                if (path === lastPath) {
+                    break;
+                }
+                lastPath = path;
+            }
+        } catch {
+            return undefined;
+        }
+    }
+}
