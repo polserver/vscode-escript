b4a1cb4bc308095358466763fd85c9dc275d71a5
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2021-08-13 19:08:30 +0200

    Introduce jump-to-definition to extension (#11)
    * Refactor HoverBuilder
    
    * Introduce DefinitionBuilder
    
    * Finalize DefinitionBuilder; add tests
    
    * Fix tests
    
    * Update polserver, package version

---
+++ native/cpp/compiler/DefinitionBuilder.h
@@ -0,0 +1,36 @@
+#ifndef VSCODEESCRIPT_DEFINITIONBUILDER_H
+#define VSCODEESCRIPT_DEFINITIONBUILDER_H
+
+#include "SemanticContextBuilder.h"
+
+namespace VSCodeEscript::CompilerExt
+{
+class DefinitionBuilder : public SemanticContextBuilder<Pol::Bscript::Compiler::SourceLocation>
+{
+public:
+  DefinitionBuilder( Pol::Bscript::Compiler::CompilerWorkspace&,
+                     const Pol::Bscript::Compiler::Position& position );
+
+  ~DefinitionBuilder() override = default;
+
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_variable(
+      std::shared_ptr<Pol::Bscript::Compiler::Variable> variable ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_constant(
+      Pol::Bscript::Compiler::ConstDeclaration* const_decl ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_module_function(
+      Pol::Bscript::Compiler::ModuleFunctionDeclaration* ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_module_function_parameter(
+      Pol::Bscript::Compiler::ModuleFunctionDeclaration* function_def,
+      Pol::Bscript::Compiler::FunctionParameterDeclaration* param ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_user_function(
+      Pol::Bscript::Compiler::UserFunction* ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_user_function_parameter(
+      Pol::Bscript::Compiler::UserFunction* function_def,
+      Pol::Bscript::Compiler::FunctionParameterDeclaration* param ) override;
+  virtual std::optional<Pol::Bscript::Compiler::SourceLocation> get_program(
+      const std::string& name, Pol::Bscript::Compiler::Program* program ) override;
+};
+
+}  // namespace VSCodeEscript::CompilerExt
+
+#endif  // VSCODEESCRIPT_DEFINITIONBUILDER_H
