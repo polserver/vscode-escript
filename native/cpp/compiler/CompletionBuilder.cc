af90c87e949cc5b06a6475c719b3e582177df6fc
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2024-10-20 11:34:40 +0200

    Update completion to support classes

--- native/cpp/compiler/CompletionBuilder.cc
+++ native/cpp/compiler/CompletionBuilder.cc
@@ -1,5 +1,10 @@
 #include "CompletionBuilder.h"
 
+#include "bscript/compiler/ast/ClassDeclaration.h"
+#include "bscript/compiler/model/ScopeName.h"
+
+#include "clib/strutil.h"
+
 using namespace Pol::Bscript::Compiler;
 using namespace EscriptGrammar;
 
@@ -12,41 +17,131 @@ CompletionBuilder::CompletionBuilder( CompilerWorkspace& workspace, const Positi
 
 std::vector<CompletionItem> CompletionBuilder::context()
 {
-  std::vector<CompletionItem> results;
-  if ( workspace.source )
+  if ( !workspace.source )
   {
-    workspace.source->accept( *this );
-    if ( !nodes.empty() )
+    return {};
+  }
+
+  workspace.source->accept( *this );
+
+  if ( nodes.empty() )
+  {
+    return {};
+  }
+
+  antlr4::Token* result = nullptr;
+  antlr4::Token* prev_token = nullptr;
+  antlr4::Token* second_prev_token = nullptr;
+  auto tokens = workspace.source->get_all_tokens();
+
+  for ( const auto& token : tokens )
+  {
+    if ( token->getLine() == position.line_number &&
+         token->getCharPositionInLine() + 1 <= position.character_column &&
+         token->getCharPositionInLine() + 1 + token->getText().length() >=
+             position.character_column )
     {
+      result = token;
+      if ( token->getTokenIndex() > 0 )
       {
-        auto result = workspace.source->get_token_at( position );
-        auto prefix = result ? result->getText().substr(
-                                   0, position.character_column - result->getCharPositionInLine() + 1 )
-                             : "";
-        for ( auto* constant : workspace.scope_tree.list_constants( prefix ) )
-        {
-          results.push_back( CompletionItem{ constant->identifier, CompletionItemKind::Constant } );
-        }
+        prev_token = tokens[token->getTokenIndex() - 1];
+      }
+      if ( token->getTokenIndex() > 1 )
+      {
+        second_prev_token = tokens[token->getTokenIndex() - 2];
+      }
+      break;
+    }
+  }
 
-        for ( auto variable : workspace.scope_tree.list_variables( prefix, position ) )
-        {
-          results.push_back( CompletionItem{ variable->name, CompletionItemKind::Variable } );
-        }
+  if ( !result )
+  {
+    return {};
+  }
 
-        for ( auto* user_function : workspace.scope_tree.list_user_functions( prefix ) )
-        {
-          results.push_back( CompletionItem{ user_function->name, CompletionItemKind::Function } );
-        }
+  ScopeTreeQuery query;
 
-        for ( auto* module_function : workspace.scope_tree.list_module_functions( prefix ) )
-        {
-          results.push_back(
-              CompletionItem{ module_function->name, CompletionItemKind::Function } );
-        }
+  query.calling_scope = calling_scope;
+
+  if ( result->getType() == EscriptLexer::IDENTIFIER )
+  {
+    query.prefix = result->getText();
+    if ( prev_token && prev_token->getType() == EscriptLexer::COLONCOLON )
+    {
+      if ( second_prev_token && second_prev_token->getType() == EscriptLexer::IDENTIFIER )
+      {
+        query.prefix_scope = second_prev_token->getText();
+      }
+      else
+      {
+        query.prefix_scope = ScopeName::Global;
       }
     }
   }
+  else if ( result->getType() == EscriptLexer::COLONCOLON )
+  {
+    if ( prev_token && prev_token->getType() == EscriptLexer::IDENTIFIER )
+    {
+      query.prefix_scope = prev_token->getText();
+    }
+    else
+    {
+      query.prefix_scope = ScopeName::Global;
+    }
+  }
+  else
+  {
+    return {};
+  }
+
+  std::vector<CompletionItem> results;
+
+  for ( auto* constant : workspace.scope_tree.list_constants( query ) )
+  {
+    results.push_back( CompletionItem{ constant->identifier, CompletionItemKind::Constant } );
+  }
+
+  for ( auto variable : workspace.scope_tree.list_variables( query, position ) )
+  {
+    results.push_back(
+        CompletionItem{ variable->scoped_name().name, CompletionItemKind::Variable } );
+  }
+
+  for ( auto* user_function : workspace.scope_tree.list_user_functions( query, position ) )
+  {
+    results.push_back( CompletionItem{ user_function->name, CompletionItemKind::Function } );
+  }
+
+  for ( auto* module_function : workspace.scope_tree.list_module_functions( query ) )
+  {
+    results.push_back( CompletionItem{ module_function->name, CompletionItemKind::Function } );
+  }
+
+  for ( auto& class_decl : workspace.scope_tree.list_classes( query ) )
+  {
+    results.push_back( CompletionItem{ class_decl->name, CompletionItemKind::Class } );
+  }
+
+  for ( auto& module_name : workspace.scope_tree.list_modules( query ) )
+  {
+    results.push_back( CompletionItem{ module_name, CompletionItemKind::Module } );
+  }
+
   return results;
+}
+
+std::any CompletionBuilder::visitClassDeclaration( EscriptParser::ClassDeclarationContext* ctx )
+{
+  if ( ctx->IDENTIFIER() )
+  {
+    Pol::Bscript::Compiler::Range range( *ctx );
+    if ( range.contains( position ) )
+    {
+      calling_scope = ctx->IDENTIFIER()->getText();
+    }
+  }
+
+  return visitChildren( ctx );
 }
 
 antlrcpp::Any CompletionBuilder::visitChildren( antlr4::tree::ParseTree* node )
